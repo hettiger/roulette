59ae61feb888ce6f8ca66d1b06a6fbac30e0599a
Martin Hettiger <martin@hettiger.com>
2018-07-06 22:49:32 +0200

    Make the number of threads configurable

--- src/Configuration.cpp
+++ src/Configuration.cpp
@@ -36,6 +36,8 @@ Configuration::Configuration(int argc, char **argv, Samplesize *samplesize) {
             verbosity = 1;
         } else if (strcmp(argv[i], "-vv") == 0) {
             verbosity = 2;
+        } else if (strncmp(argv[i], "-t=", strlen("-t=")) == 0) {
+            numThreads = (uint) stoi(((string) argv[i]).substr(strlen("-n=")));
         } else if (strncmp(argv[i], "-n=", strlen("-n=")) == 0) {
             samplesize->setSize((uint) stoi(((string) argv[i]).substr(strlen("-n="))));
         } else if (strncmp(argv[i], "-r=", strlen("-r=")) == 0) {
@@ -82,9 +84,11 @@ void Configuration::printHelp() {
     cout << "So riskierst du in einer Runde nicht zuviel..." << endl;
     cout << endl;
     cout << "Folgende Argumente können an das Programm übergeben werden:" << endl;
+    cout << endl;
     cout << "-d" << endl << "Debugging Information ausgeben." << endl << endl;
     cout << "-v" << endl << "Das Programm gibt mehr Informationen aus." << endl << endl;
     cout << "-vv" << endl << "Das Programm gibt sämtliche Informationen aus." << endl << endl;
+    cout << "-t=8" << endl << "Anzahl der Threads. (Default = 8)" << endl << endl;
     cout << "-n=1" << endl << "Anzahl der Durchläufe. (Default = 1)" << endl << endl;
     cout << "-r=200.00" << endl << "Startbankroll (Default = 200.00)" << endl << endl;
     cout << "-b=10.00" << endl << "Grundeinsatz (Default = 10.00)" << endl << endl;
